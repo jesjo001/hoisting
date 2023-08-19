18b0f4de8545835c22eb3b1ab4a1e979379a7e99
jesjo <automationlounge@gmail.com>
2023-08-19 14:13:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// console.log(x); // Outputs: undefined
+
+// var x = 10;
+// console.log(x); // Outputs: 10
+
+
+
+
+// Function hoisting
+// sayHello(); // Outputs: "Hello!"
+
+// function sayHello() {
+//   console.log("Hello!");
+// }
+
+
+// sayHi(); // Throws an error: sayHi is not a function
+
+// var sayHi = function() {
+//   console.log("Hi!");
+// };
+
+// saHello()
+
+// const saHello = () => {
+//     console.log("Hello!");
+// }
+
+
+
+// if (true) {
+//     console.log(y); // Throws an error: Cannot access 'y' before initialization
+//     var y = 20;
+// }
+
+
+if (true) {
+    console.log(y); // Throws an error: Cannot access 'y' before initialization
+    // let y = 20;
+}
